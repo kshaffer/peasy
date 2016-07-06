f2cd3f473d9a700f8215376832b66fa94ccf721c
Kris Shaffer <kris.shaffer@gmail.com>
2016-07-06 15:28:21 -0400

    Multiple page support added

--- render_page.js
+++ render_page.js
@@ -1,14 +1,32 @@
 window.onload = function() {
   var post_list_content = '';
 
-  for(i = content.posts.length - 1; i >= 0; i -= 1) {
+  // post a single page if asked via URL query
+  if (urlObject().parameters['p']) {
+    var i = urlObject().parameters['p'] - 1;
     post_list_content += '<h1>';
     post_list_content += content.posts[i].title;
-    post_list_content += '</h1>\n<h2>';
+    post_list_content += '</h1>\n<h2>by ';
     post_list_content += content.posts[i].author;
     post_list_content += '</h2>\n<br/>';
     post_list_content += content.posts[i].text;
     post_list_content += '\n<br/><hr/><br/>';
+  } else {
+
+  // post all pages in reverse index order (need to change to timestamps and reverse chronological order)
+    for(i = content.posts.length - 1; i >= 0; i -= 1) {
+      post_list_content += '<h1><a href="';
+      post_list_content += site.url;
+      post_list_content += '?p=';
+      post_list_content += (content.posts[i].index);
+      post_list_content += '">';
+      post_list_content += content.posts[i].title;
+      post_list_content += '</a></h1>\n<h2>by ';
+      post_list_content += content.posts[i].author;
+      post_list_content += '</h2>\n<br/>';
+      post_list_content += content.posts[i].text;
+      post_list_content += '\n<br/><hr/><br/>';
+  }
 }
 
   document.getElementById('list-of-posts').innerHTML = post_list_content;
